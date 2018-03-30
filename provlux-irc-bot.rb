bf393bd1150b890d0561404a0fe4523243ece1c7
cedric <cedric@studio-sept.be>
2018-03-30 09:24:23 +0200

    Fix ENV variables

--- provlux-irc-bot.rb
+++ provlux-irc-bot.rb
@@ -5,15 +5,15 @@ GREETINGS_OUTPUT = %w(Hello! Bonjour Salutations Hey! Yo Yop Bello!)
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick = BOT_NAME # Le nom du bot
+    c.nick = ENV['BOT_NAME'] # Le nom du bot
     c.realname = "Marcassin Sanglier" # Le nom réel du bot
     c.delay_joins = 5 # Un temps d'attente avant de rejoindre un chan
     c.messages_per_second = 1.0 # messages par seconde,  pour éviter de se faire kick pour flood
     c.server_queue_size = 1 # Un message par cible pour du round robin, évite de se faire kick
     c.server = "irc.freenode.org"
     c.channels = ["#provlux"]
-    c.sasl.username = BOT_NAME
-    c.sasl.password = BOT_PASSWORD
+    c.sasl.username = ENV['BOT_NAME']
+    c.sasl.password = ENV['BOT_PASSWORD']
   end
 
   on :message, GREETINGS_INPUT_REGEXP do |m|
@@ -24,7 +24,7 @@ bot = Cinch::Bot.new do
     m.reply 'pong'
   end
 
-  on :message, /#{BOT_NAME}( )?(\?)?/ do |m|
+  on :message, /#{ENV['BOT_NAME']}( )?(\?)?/ do |m|
     m.reply 'Oui?'
   end
 
